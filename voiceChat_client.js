87f2e25d1862e461ebd3a18e14846ddb8095ecdd
Yanick <rt-2@rt-2.net>
2017-10-17 18:08:37 -0400

    Create voiceChat_client.js

---
+++ voiceChat_client.js
@@ -0,0 +1,263 @@
+//
+// Client-side version of the voiceChat feature
+//
+
+
+
+//
+//  Variable(s)  BEGIN
+//
+
+
+
+// Constant(s)
+const VOICECHAT_MAX_HEAR_DIST = 35.0;
+const VOICECHAT_PROX_TIMER_INTERVAL = 1000;
+const VOICECHAT_VOL_TIMER_INTERVAL = 120;
+
+// Execution var(s)
+let player_isVoicable = new Array(MAX_PLAYERS);
+let player_isStreamingOtherPlayer = new Array(MAX_PLAYERS);
+let player_isOtherPlayerReady = new Array(MAX_PLAYERS);
+let player_isCloseToOtherPlayer = new Array(MAX_PLAYERS);
+let player_lastVolumeSetForPlayer = new Array(MAX_PLAYERS);
+
+// Entitie(s)
+
+// Browser(s)
+let voiceChat_browser = null;
+
+
+//
+//  Variable(s)  END
+//
+
+
+//
+//  Timer(s)  BEGIN
+//
+
+function voiceChat_volumeTimer(other_player) {
+        
+    try {
+    if (
+        other_player.type == 'player' &&
+        player_isCloseToOtherPlayer[other_player.id]
+        ) {
+
+        // Init 
+        let player = mp.players.local;
+
+        if (Functions.IsPlayerValid(player)) {
+
+
+            let distance = Functions.PosDistanceFromPos(other_player.position, player.position);
+
+
+            if (
+                Functions.IsPlayerValid(player) &&
+                distance < VOICECHAT_MAX_HEAR_DIST
+            ) {
+
+                let volume = 1.0 / VOICECHAT_MAX_HEAR_DIST * distance;
+                volume = volume * -1 + 1.0;
+
+                // Only set if data is new
+                if (volume != player_lastVolumeSetForPlayer[other_player.id]) {
+
+                    player_lastVolumeSetForPlayer[other_player.id] = volume;
+                    voiceChat_browser.execute('SetOtherPlayerLevel(' + other_player.id + ', "' + volume + '");');
+                }
+
+
+
+            }
+            else {
+                if (player_lastVolumeSetForPlayer[other_player.id] != 0.0) {
+                    voiceChat_browser.execute('SetOtherPlayerLevel(' + other_player.id + ', "0.0");');
+                }
+            }
+        }
+
+        setTimeout(function () { voiceChat_volumeTimer(other_player); }, VOICECHAT_VOL_TIMER_INTERVAL);
+
+    }
+
+    }
+    catch (err) {
+        mp.gui.chat.push('!{yellow}The error: !{white}' + err.message);
+    }
+
+}
+
+function voiceChat_proximityTimer(other_player) {
+
+
+    try {
+        if (other_player) {
+
+            if (other_player.type == "player") {
+
+                if (player_isStreamingOtherPlayer[other_player.id]) {
+
+                    // Init 
+                    let player = mp.players.local;
+
+
+
+                    let distance = Functions.PosDistanceFromPos(other_player.position, player.position);
+
+
+                    if (distance < 100.0) {
+
+                        // MOVED
+                        if (
+                            !player_isCloseToOtherPlayer[other_player.id] &&
+                            player_isOtherPlayerReady[other_player.id]
+                        ) {
+
+
+                            // Actions
+
+                            player_isCloseToOtherPlayer[other_player.id] = true;
+
+
+                            mp.gui.chat.push('InitCall(' + other_player.id + '); at ' + Date.now() + '');
+                            voiceChat_browser.execute('InitCall(' + other_player.id + ');');
+
+                            voiceChat_volumeTimer(other_player);
+
+                        }
+
+                    }
+                    else {
+                        if (player_isCloseToOtherPlayer[other_player.id]) {
+                            // Actions
+                            player_isCloseToOtherPlayer[other_player.id] = false;
+                        }
+                    }
+
+                    setTimeout(function () { voiceChat_proximityTimer(other_player); }, VOICECHAT_PROX_TIMER_INTERVAL);
+
+                }
+            }
+            else {
+                mp.gui.chat.push('voiceChat_proximityTimer_test out of \'other_player\';');
+
+            }
+        }
+    }
+    catch (err) {
+        mp.gui.chat.push('!{yellow}The error: !{white}' + err.message);
+    }
+
+}
+
+//
+//  Timer(s)  END
+//
+
+
+//
+//  Events(s)  BEGIN
+//
+
+// Client Init
+
+mp.events.add('guiReady', () => {
+
+
+    // Init 
+    let player = mp.players.local;
+
+    // Var(s) 
+    for (let i = 0; i < MAX_PLAYERS; i++) {
+
+        player_isOtherPlayerReady[i] = false;
+        player_isStreamingOtherPlayer[i] = false;
+        player_isCloseToOtherPlayer[i] = false;
+    }
+
+    // Action(s)
+    voiceChat_browser = mp.browsers.new('https://v.sa-arp.net/ig_assets/voiceChat/iframes.html?player_id=' + player.id + '&MAX_PLAYERS=' + MAX_PLAYERS);
+    voiceChat_browser.active = true;
+
+});
+
+// Other player streams in
+mp.events.add('entityStreamIn', (entity) => {
+
+    // Init 
+    let player = mp.players.local;
+
+    if (Functions.IsPlayerValid(player)) {
+
+        if (entity.type == "player") {
+
+            // Init
+            let other_player = entity;
+
+            mp.gui.chat.push('Created')
+
+            // Actions
+            player_isOtherPlayerReady[other_player.id] = false;
+            player_isStreamingOtherPlayer[other_player.id] = true;
+
+            mp.gui.chat.push('AddOtherPlayerInRange(' + other_player.id + ', "' + other_player.name + '"); at ' + Date.now() + '');
+            voiceChat_browser.execute('AddOtherPlayerInRange(' + other_player.id + ', "' + other_player.name + '");');
+            
+            mp.gui.chat.push('Adding voiceChat_proximityTimer(' + other_player.id + '); at ' + Date.now() + '');
+            voiceChat_proximityTimer(other_player);
+
+        }
+    }
+});
+
+//
+mp.events.add('voiceChat_iframeReadyToInitOtherPlayer', (other_player_id) => {
+
+
+    // Actions
+    mp.events.callRemote("voiceChat_PlayerStreamPlayerIn", other_player_id);
+
+});
+
+//
+mp.events.add('voiceChat_playerReadyToInitOtherPlayer', (other_player_id) => {
+
+
+    // Actions
+    player_isOtherPlayerReady[other_player_id] = true;
+
+});
+
+// Other player streams out
+mp.events.add('entityStreamOut', (entity) => {
+
+    if (entity.type == "player") {
+
+        // Init
+        let other_player = entity;
+
+        // Actions
+        player_isStreamingOtherPlayer[other_player.id] = false;
+
+        mp.gui.chat.push('RemoveOtherPlayerInRange(' + other_player.id + '); at ' + Date.now() + '');
+        voiceChat_browser.execute('RemoveOtherPlayerInRange(' + other_player.id + ');');
+
+        mp.events.callRemote("voiceChat_PlayerStreamPlayerOut", other_player);
+
+    }
+});
+
+// Receive and echo any browser errors
+mp.events.add('voiceChat_browserError', (error_text) => {
+
+    mp.gui.chat.push('!{F00}Browser error: !{FFF}' + error_text);
+
+});
+
+//
+//  Events(s)  END
+//
+
